703675266a287dadd1869a88f6cdb8a807bae5eb
DevsGVA <contato@globalvisionaccess.com>
2026-07-07 16:45:40 -0300

    fix: update CSS path to backstage/assets/admin.css

--- backstage/includes/sidebar.php
+++ backstage/includes/sidebar.php
@@ -14,7 +14,7 @@ $isSuperAdmin = ehSuperAdmin();
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/tinymce@7/tinymce.min.js" referrerpolicy="origin"></script>
-  <link rel="stylesheet" href="<?= $adminBase ?>../admin/assets/admin.css?v=<?= filemtime(__DIR__ . '/../../admin/assets/admin.css') ?>">
+  <link rel="stylesheet" href="<?= $adminBase ?>assets/admin.css?v=<?= filemtime(__DIR__ . '/../assets/admin.css') ?>">
 </head>
 <body>
 
